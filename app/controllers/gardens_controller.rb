e28c4170419e6c3936615715c167bfd41004a91a
Andrea Freeman <afreeman5566@gmail.com>
2021-12-06 12:12:10 -0600

    garden controller added

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,20 @@
 class GardensController < ApplicationController
+    def show
+        garden = Garden.all 
+        render json: garden
+
+    end
+
+    def index
+        plant = Garden.create(garden_params)
+        render json: plant 
+
+    end
+
+    private
+
+    def garden_params
+    params.permit(:name, :location )
+    end
+end
 end
