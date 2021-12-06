804d3593f15012df3d5db8f2c6286df7ecbea29b
Andrea Freeman <afreeman5566@gmail.com>
2021-12-06 17:48:34 -0600

    form buttons styled

--- client/src/Components/PlantCard.js
+++ client/src/Components/PlantCard.js
@@ -10,8 +10,8 @@ function PlantCard({plant}) {
                 <Card.Title> {plant.name}</Card.Title>
                 <Card.Subtitle>{plant.plant_type}</Card.Subtitle>
                 <Card.Text>{plant.notes}</Card.Text>
-                <button class ="crud">Edit</button>
-                <button class ="crud">Delete</button>
+                <button class ="redirect_btn">Edit</button>
+                <button class ="redirect_btn">Delete</button>
             </Card.Body>
         {console.log(plant)}
         </Card>
