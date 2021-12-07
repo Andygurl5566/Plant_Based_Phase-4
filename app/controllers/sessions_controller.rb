19877ead191afe17faecbd517468f3b9c1ffa913
James Brownie <jamespbrownie@gmail.com>
2021-12-07 14:13:37 -0600

    sessions and users controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,37 @@
 class SessionsController < ApplicationController
-  # skip_before_action :authenticate_user, only: [:create]
+  skip_before_action :authorize, only: :create
 
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: user, status: :ok
+      render json: user
     else 
-      render json: "Invalid Credentials", status: :unauthorized
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
     end
   end
 
   def destroy
     session.delete :user_id
+    head :no_content
   end
 
 end
+# class SessionsController < ApplicationController
+#   # skip_before_action :authenticate_user, only: [:create]
+
+#   def create
+#     user = User.find_by(username: params[:username])
+#     if user&.authenticate(params[:password])
+#       session[:user_id] = user.id
+#       render json: user, status: :ok
+#     else 
+#       render json: "Invalid Credentials", status: :unauthorized
+#     end
+#   end
+
+#   def destroy
+#     session.delete :user_id
+#   end
+
+# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,64 @@
 class UsersController < ApplicationController
-    
-    # skip_before_action :authenticate_user, only: [:create, :show]
-
-    def index
-        render json: User.all, each_serializer: UserSerializer
-    end
-
-
-
-    def show
-        if current_user
-            render json: current_user, status: :ok
-        else 
-            render json: {error: "No current session stored"}, status: :unauthorized
-        end
-    end
+    skip_before_action :authorize, only: :create
 
     def create 
-        user = User.create(user_params)
-        if user.valid?
-            session[:user_id] = user.id
-            render json: user, status: :created
-        else 
-            render json: {user.errors.full_messages}, 
-            status: :unprocessable_entity
-        end
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
     end
 
-    def destroy
-        user = User.find_by(id: params[:id])
-        if user
-            user.destroy
-            head :no_content
-        end
+    def show
+        render json: @current_user
     end
 
     private
 
     def user_params 
-        params.permit(:name, :email, :password, :bio, :garden_level)
+        params.permit(:username, :password, :name, :email, :bio, :garden_level)
     end
 
 end
+# class UsersController < ApplicationController
+    
+#     # skip_before_action :authenticate_user, only: [:create, :show]
+
+#     def index
+#         render json: User.all, each_serializer: UserSerializer
+#     end
+
+
+
+#     def show
+#         if current_user
+#             render json: current_user, status: :ok
+#         else 
+#             render json: {error: "No current session stored"}, status: :unauthorized
+#         end
+#     end
+
+#     def create 
+#         user = User.create(user_params)
+#         if user.valid?
+#             session[:user_id] = user.id
+#             render json: user, status: :created
+#         else 
+#             render json: {user.errors.full_messages}, 
+#             status: :unprocessable_entity
+#         end
+#     end
+
+#     def destroy
+#         user = User.find_by(id: params[:id])
+#         if user
+#             user.destroy
+#             head :no_content
+#         end
+#     end
+
+#     private
+
+#     def user_params 
+#         params.permit(:name, :email, :password, :bio, :garden_level)
+#     end
+
+# end
